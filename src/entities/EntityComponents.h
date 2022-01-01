1e6fcf47d532bf695ec637e60643817e4d136e70
Shalev-Lifshitz <lifshitz.shalev@gmail.com>
2022-01-01 03:18:20 -0500

    Added EntityComponents.h (defines component types)

---
+++ src/entities/EntityComponents.h
@@ -0,0 +1,17 @@
+#ifndef PSR_3D_RENDERER_ENTITYCOMPONENTS_H
+#define PSR_3D_RENDERER_ENTITYCOMPONENTS_H
+
+#include <glm/glm.hpp> // vec2, vec3, mat4, radians
+#include <opencv2/opencv.hpp>
+
+// Define an EntityID type
+using EntityID = int64_t;
+
+// Define the type of each component map
+template <typename Type>
+using ComponentMap = std::unordered_map<EntityID, Type>;
+using Positions = ComponentMap<glm::vec3>;
+using Orientations = ComponentMap<glm::vec3>;
+using Meshes = ComponentMap<cv::Mat>; // Each Mat has shape numEntities x numTriangles x 3
+
+#endif //PSR_3D_RENDERER_ENTITYCOMPONENTS_H
