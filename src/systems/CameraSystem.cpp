a7f5def00d5d9d8a9afd11565d5e3be0e1205bf8
Mogtaba-Alim <alim.mogtaba@gmail.com>
2022-01-05 00:44:40 -0500

    updated camera system so position is only updated if wasd is selected and added multiplyer and switched w and a and s and d functionality

--- src/systems/CameraSystem.cpp
+++ src/systems/CameraSystem.cpp
@@ -6,34 +6,44 @@ CameraSystem::CameraSystem(int cameraViewAngle)
 
 void CameraSystem::updateCamera(int keycode, glm::vec2 mousePosition) {
     // Determine how to update camera position based on key pressed
+    bool updatedPosition = false;
+    float mult = 0.01;
     glm::vec3 positionUpdate;
     switch (keycode) {
         case 119: // 'w' - FORWARD
-            positionUpdate = glm::vec3(1, 0, 0);
+            positionUpdate = glm::vec3(0, 0, -1);
+            updatedPosition = true;
             break;
         case 115: // 's' - BACKWARD
-            positionUpdate = glm::vec3(-1, 0, 0);
+            positionUpdate = glm::vec3(0, 0, 1);
+            updatedPosition = true;
             break;
         case 97: // 'a' - LEFT
-            positionUpdate = glm::vec3(0, 0, -1);
+            positionUpdate = glm::vec3(1 * mult, 0, 0);
+            updatedPosition = true;
             break;
         case 100: // 'd' - RIGHT
-            positionUpdate = glm::vec3(0, 0, 1);
+            positionUpdate = glm::vec3(-1 * mult, 0, 0);
+            updatedPosition = true;
             break;
         case 32: // 'SPACE' - UP
-            positionUpdate = glm::vec3(0, 1, 0);
+            positionUpdate = glm::vec3(0, 1 * mult, 0);
+            updatedPosition = true;
             break;
         case 9: // 'TAB' - DOWN
-            positionUpdate = glm::vec3(0, -1, 0);
+            positionUpdate = glm::vec3(0, -1 * mult, 0);
+            updatedPosition = true;
             break;
     }
+    if (updatedPosition ){
+        // Compute new position and update camera
+        glm::vec3 newCameraPosition = camera.getPosition() + positionUpdate;
+        camera.setPosition(newCameraPosition);
+    }
 
-    // Compute new position and update camera
-    glm::vec3 newCameraPosition = camera.getPosition() + positionUpdate;
-    camera.setPosition(newCameraPosition);
 
     // TODO: Update camera orientation
-    setCameraOrientation(mousePosition);
+//    setCameraOrientation(mousePosition);
 }
 
 void CameraSystem::setCameraOrientation(const glm::vec2& mousePosition) {
