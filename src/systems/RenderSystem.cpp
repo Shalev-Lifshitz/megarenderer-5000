dbdd90645f41cc718e781e95484424ad7921437c
Mogtaba-Alim <alim.mogtaba@gmail.com>
2022-01-05 17:38:35 -0500

    fixed coordinate in camera view

--- src/systems/RenderSystem.cpp
+++ src/systems/RenderSystem.cpp
@@ -133,7 +133,7 @@ std::unique_ptr<cv::Mat> RenderSystem::renderScene(cv::Mat &imageBackground, lon
     glm::mat4 view;
 
 
-    glm::vec3 cameraPos   = cameraSystem.getCameraPosition();
+    glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);;
     glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 
