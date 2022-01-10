2f7b226e745eb9c5b9ebe23db0849d9dc8279e83
Shalev-Lifshitz <lifshitz.shalev@gmail.com>
2022-01-09 22:40:57 -0500

    Added testing option to main

--- src/drivers/main.cpp
+++ src/drivers/main.cpp
@@ -1,18 +1,25 @@
 #include <opencv2/opencv.hpp>
 #include "Game.h"
+#include "../math/TestLinearAlgebraMath.h"
+#include "glm/gtx/vector_angle.hpp"
 
 int main(int argc, char **argv) {
-    std::string backgroundImagePath = "./images/3d-mesh.jpg";
-    int cameraViewAngle = 90;
-    int screenHeight = 800;
-    int screenWidth = 800;
+    bool runTests = false;
+    if (runTests) {
+        TestLinearAlgebraMath::testAll();
+    } else {
+        std::string backgroundImagePath = "./images/3d-mesh.jpg";
+        int cameraViewAngle = 90;
+        int screenHeight = 800;
+        int screenWidth = 800;
 
-    CameraSystem cameraSystem(cameraViewAngle);
-    EntitySystem entitySystem(cameraSystem);
-    RenderSystem renderSystem(entitySystem, cameraSystem, screenHeight, screenWidth);
-    Game game(cameraSystem, entitySystem, renderSystem);
-    bool success = game.runGameLoop(backgroundImagePath, screenHeight, screenWidth);
+        CameraSystem cameraSystem(cameraViewAngle, screenHeight, screenWidth);
+        EntitySystem entitySystem(cameraSystem);
+        RenderSystem renderSystem(entitySystem, cameraSystem, screenHeight, screenWidth);
+        Game game(cameraSystem, entitySystem, renderSystem);
+        bool success = game.runGameLoop(backgroundImagePath, screenHeight, screenWidth);
 
-    if (success) { return 0; }
-    else { return -1; }
+        if (success) { return 0; }
+        else { return -1; }
+    }
 }
